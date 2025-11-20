9232793b867d8aaec6137127a2766378f9d2f3ac
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-11-19 21:24:42 -0300

    fix: Corrige valor del estado del reporte

--- src/core/invocations/dto/report-status.enum.ts
+++ src/core/invocations/dto/report-status.enum.ts
@@ -11,7 +11,7 @@
  */
 export enum ReportStatus {
   /** Análisis exitoso */
-  OPEN = 'SUCCESS',
+  COMPLETED = 'COMPLETED',
 
   /** Análisis falló o errores críticos encontrados */
   FAILED = 'FAILED',
